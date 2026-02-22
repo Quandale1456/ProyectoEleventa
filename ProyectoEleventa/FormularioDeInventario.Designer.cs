be906afeb4ba61a9764f32df8a91bedef39c16c1
Quandale1456 <hostynxo1234@gmail.com>
2026-02-22 12:54:24 -0400

    diseño de inv hecho 30%

---
+++ ProyectoEleventa/FormularioDeInventario.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ProyectoEleventa
+{
+    partial class FormularioDeInventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReporteDeMovimientos = new System.Windows.Forms.Button();
+            this.btnReporteDeInv = new System.Windows.Forms.Button();
+            this.btnProductoBajos = new System.Windows.Forms.Button();
+            this.btnAjustes = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodigoProducto = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panelHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnReporteDeMovimientos);
+            this.panel1.Controls.Add(this.btnReporteDeInv);
+            this.panel1.Controls.Add(this.btnProductoBajos);
+            this.panel1.Controls.Add(this.btnAjustes);
+            this.panel1.Controls.Add(this.btnAgregar);
+            this.panel1.Location = new System.Drawing.Point(0, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1483, 43);
+            this.panel1.TabIndex = 27;
+            // 
+            // btnReporteDeMovimientos
+            // 
+            this.btnReporteDeMovimientos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnReporteDeMovimientos.Location = new System.Drawing.Point(536, 6);
+            this.btnReporteDeMovimientos.Name = "btnReporteDeMovimientos";
+            this.btnReporteDeMovimientos.Size = new System.Drawing.Size(143, 30);
+            this.btnReporteDeMovimientos.TabIndex = 26;
+            this.btnReporteDeMovimientos.Text = "Reporte De Movimientos";
+            this.btnReporteDeMovimientos.UseVisualStyleBackColor = true;
+            // 
+            // btnReporteDeInv
+            // 
+            this.btnReporteDeInv.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnReporteDeInv.Location = new System.Drawing.Point(404, 6);
+            this.btnReporteDeInv.Name = "btnReporteDeInv";
+            this.btnReporteDeInv.Size = new System.Drawing.Size(126, 30);
+            this.btnReporteDeInv.TabIndex = 28;
+            this.btnReporteDeInv.Text = "Reporte de Inventario";
+            this.btnReporteDeInv.UseVisualStyleBackColor = true;
+            // 
+            // btnProductoBajos
+            // 
+            this.btnProductoBajos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnProductoBajos.Location = new System.Drawing.Point(229, 6);
+            this.btnProductoBajos.Name = "btnProductoBajos";
+            this.btnProductoBajos.Size = new System.Drawing.Size(169, 30);
+            this.btnProductoBajos.TabIndex = 29;
+            this.btnProductoBajos.Text = "Productos Bajos en Inventario";
+            this.btnProductoBajos.UseVisualStyleBackColor = true;
+            // 
+            // btnAjustes
+            // 
+            this.btnAjustes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAjustes.Location = new System.Drawing.Point(121, 6);
+            this.btnAjustes.Name = "btnAjustes";
+            this.btnAjustes.Size = new System.Drawing.Size(102, 30);
+            this.btnAjustes.TabIndex = 30;
+            this.btnAjustes.Text = "Ajustes";
+            this.btnAjustes.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAgregar.Location = new System.Drawing.Point(12, 6);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(102, 30);
+            this.btnAgregar.TabIndex = 31;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panelHeader.Controls.Add(this.labelTitle);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(1483, 36);
+            this.panelHeader.TabIndex = 26;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.labelTitle.ForeColor = System.Drawing.Color.White;
+            this.labelTitle.Location = new System.Drawing.Point(8, 8);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(135, 24);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "INVENTARIO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(12, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "AGREGAR INVENTARIO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 16);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Codigo del Producto";
+            // 
+            // txtCodigoProducto
+            // 
+            this.txtCodigoProducto.Location = new System.Drawing.Point(150, 146);
+            this.txtCodigoProducto.Name = "txtCodigoProducto";
+            this.txtCodigoProducto.Size = new System.Drawing.Size(174, 20);
+            this.txtCodigoProducto.TabIndex = 30;
+            // 
+            // FormularioDeInventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1483, 724);
+            this.Controls.Add(this.txtCodigoProducto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormularioDeInventario";
+            this.Text = "FormularioDeInventario";
+            this.panel1.ResumeLayout(false);
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnReporteDeMovimientos;
+        private System.Windows.Forms.Button btnReporteDeInv;
+        private System.Windows.Forms.Button btnProductoBajos;
+        private System.Windows.Forms.Button btnAjustes;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodigoProducto;
+    }
+}
